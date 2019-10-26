ef2e28352d828a6920ef003672f533db94f9c397
kade <potter.kade@gmail.com>
2019-10-26 16:33:29 +0700

    controller clearing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Wavegame Adminer
                 </div>
 
                 <div class="links">
