55aff27a9ae30ca34a76016cd7a3f7204b328d94
kade <potter.kade@gmail.com>
2019-10-26 15:47:44 +0700

    first

---
+++ app/Http/Controllers/DosenController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DosenController extends Controller
+{
+    public function index()
+    {
+        $nama="Wahyu Kusuma Dewi";
+        $pelajaran = ["Algoritma & Pemrograman","Kalkulus","Pemrograman Web"];
+    	return view('biodata',['nama'=>$nama, 'matkul'=>$pelajaran]);
+    }
+    //di sini isi controller pegawai
+}
